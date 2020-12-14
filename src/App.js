7ce350a12a375178488ca7d2779bb8316affe86c
Gordon Krieger <gordon.krieger@gmail.com>
2020-12-13 22:20:37 -0500

    fix project name

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import PhaseOScope from './components/PhaseOScope'
-import {GlobalStyle} from './GlobalStyle'
-import './App.css';
+import PhaseOScope from "./components/PhaseOScope";
+import { GlobalStyle } from "./GlobalStyle";
+import "./App.css";
 
 function App() {
-  return (<>
-    <GlobalStyle/>
-  <PhaseOScope/>
-  </>
+  return (
+    <>
+      <GlobalStyle />
+      <PhaseOScope />
+    </>
   );
 }
 
